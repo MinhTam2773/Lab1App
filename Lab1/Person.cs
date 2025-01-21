5bef20f78fe1940df4224c693c44c5d35da9ae17
Minh Tam Nguyen <tamnguyen277353@gmail.com>
2025-01-21 12:59:20 -0700

    Add project files.

---
+++ Lab1/Person.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public class Person
+    {
+        //private data
+        private int personId;
+        private string firstName;
+        private string lastName;
+        private string favoriteColor;
+        private int age;
+        private bool isWorking;
+
+        //public properties
+        public string FirstName { get { return firstName; } }
+        public double Age { get { return age; } }
+        public string FavoriteColor { get { return favoriteColor; } }
+
+        //constructor
+        public Person(int personId, string firstName, string lastName, string favoriteColor, int age, bool isWorking)
+        {
+            this.personId = personId;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.favoriteColor = favoriteColor;
+            this.age = age;
+            this.isWorking = isWorking;
+        }
+
+
+        //public methods
+
+        /// <summary>
+        /// Methods for getting name = first name + last name
+        /// </summary>
+        public string GetName()
+        {
+            return firstName + " " + lastName;
+        }
+
+        /// <summary>
+        /// display information: personID, name(first name + last name), favorite color 
+        /// </summary>
+        public string DisplayPersonInfo()
+        {
+            string name = this.GetName();
+            return $"{personId}: {name}'s favorite colour is {favoriteColor}";
+        }
+
+        /// <summary>
+        /// change the object's favorite color to white
+        /// </summary>
+        public void ChangeFavoriteColour()
+        {
+            favoriteColor = "White";   
+        }
+
+        /// <summary>
+        /// return object's age after 10 years
+        /// </summary>
+        public int GetAgeInTenYears()
+        {
+            return age + 10;
+        }
+
+        public override string ToString()
+        {
+            string name = this.GetName();
+            return  $"PersonId: {personId}\n" +
+                    $"FirstName: {firstName}\n" +
+                    $"LastName: {lastName}\n" +
+                    $"FavoriteColour: {favoriteColor}\n" +
+                    $"Age: {age}\n" +
+                    $"IsWorking: {isWorking}";
+        }
+    }
+}
---
+++ Lab1/Relation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    // enumeration types for relationship types
+    public enum RelationshipType { Sister, Brother, Mother, Father };
+    public class Relation
+    {
+        //private data
+        private RelationshipType r; 
+
+        //constructor
+        public  Relation(RelationshipType r)
+        {
+            this.r = r;
+        }
+
+        //public method
+        public string ShowRelationShip( Person person1, Person person2)
+        {
+            return $"Relationship between {person1.FirstName} and {person2.FirstName} is: {r}hood";
+        }
+    
+
+    }
+}
